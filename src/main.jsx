ac70666ef4180a0b79e6e493c3ea36e2ad7b1ac7
Paul Cristerna <olivierpaulcristerna@gmail.com>
2024-09-03 14:04:00 -0700

    Main files.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Checker from './pages/Checker'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Checker />
   </StrictMode>,
 )
---
+++ src/pages/Checker.jsx
@@ -0,0 +1,10 @@
+import './Checker.css'
+
+function App() {
+
+  return (
+    <h1>Price Checker</h1>
+  )
+}
+
+export default App
